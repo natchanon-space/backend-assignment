3d74aaf427a3e62567f3c42f9358d6521b7f77c1
root <root@LAPTOP-VSV3T7OA.localdomain>
2022-02-06 15:23:39 +0700

    complete GET method

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurants"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
+
+mylist = client.list_database_names()
+print(mylist)
 
 app = FastAPI()
 
@@ -21,11 +24,21 @@ app = FastAPI()
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    result = collection.find_one({"name": name})
+    return {
+        "name": result["name"],
+        "table": result["table"],
+        "time": result["time"]
+    }
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    result = collection.find_one({"table": table})
+    return {
+        "name": result["name"],
+        "table": result["table"],
+        "time": result["time"]
+    }
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
